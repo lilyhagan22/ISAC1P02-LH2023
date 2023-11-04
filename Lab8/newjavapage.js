bda300572f5080fa671f345bceed1a7bcb88b47f
lilyhagan22 <144627901+lilyhagan22@users.noreply.github.com>
2023-11-04 18:29:30 -0400

    new lab assignment

---
+++ Lab8/newjavapage.js
@@ -0,0 +1,24 @@
+var numX, numY;
+numX=0;
+numY=0;
+
+var numZ=0;
+
+document.write("Hello World!");
+console.log("wrote hello world");
+
+function function1(){
+  document.getElementByID("f1Ref").innerHTML="Looke it worked";
+  document.getElementByID("f1Ref").style.color="green";
+
+  console.log("made f1 contents green and overwrote them");
+}
+
+function changeIntro(){
+  document.getElementByID("annoy").style.color="red";
+  console.log("made my annoying into red");
+}
+
+function addxandy(){
+
+}
