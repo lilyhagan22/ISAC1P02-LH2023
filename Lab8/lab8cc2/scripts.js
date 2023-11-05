8fcd255fc8afb33d28ce0a797d2a3bba64ef04b1
lilyhagan22 <144627901+lilyhagan22@users.noreply.github.com>
2023-11-05 18:37:35 -0500

    updates

--- Lab8/lab8cc2/scripts.js
+++ Lab8/lab8cc2/scripts.js
@@ -7,10 +7,10 @@ document.getElementById("red").style.color="red";
 function mix(){
 
 	var first = "#00BB00";
-	var second = "BB9320";
+	var second ="#BB9320";
 	var third = "#AB00FF";
 
 	document.getElementById("blue").style.color=first;
-	document.getElementById("green").style.colour=second;
+	document.getElementById("green").style.color=second;
 	document.getElementById("red").style.color=third;
 }
