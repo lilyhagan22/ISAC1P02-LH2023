318abaa8f783ef2395d40e124ea1c6b0569afa5c
lilyhagan22 <144627901+lilyhagan22@users.noreply.github.com>
2023-12-14 20:47:08 -0500

    updates

--- FinalWebsite/occipitallobepage.js
+++ FinalWebsite/occipitallobepage.js
@@ -1,3 +1,34 @@
-function formOutputs(){
-  console.log(document.getElementByName("checkInput").value);
+function formOutputs1(){
+  console.log(document.getElementById("basicText1").value);
 }
+
+if (basicText1 == "Bunny"){
+  document.write("I agree");
+}else{
+   document.write("I do not agree");
+
+   document.getElementById("answer1").innerHTML
+}
+
+function formOutputs2(){
+  console.log(document.getElementById("basicText2").value);
+}
+
+if (basicText2 == "Old Couple"){
+    document.write("I agree");
+}else{
+     document.write("I do not agree");
+
+    document.getElementById("answer2").innerHTML
+  }
+
+function formOutputs3(){
+  console.log(document.getElementById("basicText3").value);
+}
+  if (basicText3 == "Frog"){
+    document.write("I agree");
+  }else{
+     document.write("I do not agree");
+
+    document.getElementById("answer3").innerHTML
+  }
---
+++ FinalWebsite/parietallobe.js
@@ -0,0 +1,25 @@
+var x = prompt("How old are you?", "0");
+var num1 = parseInt(x);
+console.log("Age:" + num1);
+
+var todayFullDate = new Date();
+var todayTime = todayFullDate.getTime();
+var todayMonth = todayFullDate.getMonth() + 1; //Fix for the zero based index
+var todayDate = todayFullDate.getDate();
+var todayDay = todayFullDate.getDay();
+var todayHour = todayFullDate.getHours();
+var todayYear = todayFullDate.getYear();
+var todayFullYear = todayFullDate.getFullYear();
+
+console.log("Full Date:" + todayFullDate);
+console.log("Time:" + todayTime);
+console.log("Month" + todayMonth);
+console.log("Date:" + todayDate);
+console.log("Day:" + todayDay);
+console.log("Hour:" + todayHour);
+console.log("Year:" + todayYear);
+console.log("FullYear:" + todayFullYear);
+
+
+document.getElementById("result").innerHTML=(todayFullYear - num1);
+console.log(todayFullYear - num1);
