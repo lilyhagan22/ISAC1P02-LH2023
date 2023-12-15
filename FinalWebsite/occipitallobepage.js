c33eef8de1a20445f6d12066f3a7627c4082f359
lilyhagan22 <144627901+lilyhagan22@users.noreply.github.com>
2023-12-15 15:04:23 -0500

    updates

--- FinalWebsite/occipitallobepage.js
+++ FinalWebsite/occipitallobepage.js
@@ -1,35 +1,43 @@
 function formOutputs1(){
   console.log(document.getElementById("basicText1").value);
-  document.getElementById("answer1").innerHTML
-}
+  var basicText1js = document.getElementById('basicText1').value;
 
-if (basicText1 == "Bunny"){
-  document.write("I agree");
+document.getElementById('answer1box').innerHTML= "newstuff"
+
+if (basicText1js == "Bunny"){
+  document.write("I agree").innerHTML = "newstuff"
+
 }else{
-   document.write("I do not agree");
+   document.write("I do not agree").innerHTML ="newstuff";
 
-
+}
 }
 
 function formOutputs2(){
   console.log(document.getElementById("basicText2").value);
-}
+  var basicText2js = document.getElementById('basicText2').value;
 
-if (basicText2 == "Old Couple"){
+
+if (basicText2js == "Old Couple"){
     document.write("I agree");
 }else{
      document.write("I do not agree");
 
-    document.getElementById("answer2").innerHTML
+    document.getElementById("answer2").innerHTML;
   }
+}
+
 
 function formOutputs3(){
   console.log(document.getElementById("basicText3").value);
-}
-  if (basicText3 == "Frog"){
+  var basicText3js = document.getElementById('basicText3').value;
+
+
+  if (basicText3js == "Frog"){
     document.write("I agree");
   }else{
      document.write("I do not agree");
 
-    document.getElementById("answer3").innerHTML
+
   }
+}
